7047963d2fbde2966395fa3addcaecf3a1c3baa0
Kevin Cho <chokevinj@gmail.com>
2023-08-27 17:42:11 -0400

    add reselect to memoize selectors

--- src/store/cart/selectors.js
+++ src/store/cart/selectors.js
@@ -1,2 +1,16 @@
+import { createSelector } from 'reselect'
 
-export const getCartQuantity = state => state.cart.items.length || 0;
+const getItems = state => state.cart.items;
+
+const getIsOpen = state => state.cart.isOpen;
+
+export const getCartQuantity = createSelector(getItems, items => items.length || 0);
+
+export const getCartItems = createSelector(getItems, items => items);
+
+export const getCartTotal = createSelector(
+  getItems,
+  items => items.reduce((acc, curr) => acc + curr.price, 0),
+)
+
+export const getCartIsOpen = createSelector(getIsOpen, isOpen => isOpen);
